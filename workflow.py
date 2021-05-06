67fbd6ff773969200c310e3d0f1d50a9c43426da
Mike Melanson <mike@multimedia.cx>
2021-05-06 09:19:33 -0700

    Find the straight edges and rotate

--- workflow.py
+++ workflow.py
@@ -1,9 +1,11 @@
 import sys
 
 import cv2 as cv
+import numpy as np
 
-KEY_ESC = 27
 KEY_ENTER = 13
+KEY_ESC = 27
+KEY_SPACE = 32
 KEY_LEFT_ARROW = 0x250000
 KEY_UP_ARROW = 0x260000
 KEY_RIGHT_ARROW = 0x270000
@@ -54,7 +56,6 @@ if __name__ == '__main__':
     analyzerImageGray = cv.cvtColor(analyzerImage, cv.COLOR_BGR2GRAY)
     minRadius = int(ANALYSIS_SIZE/2 * 0.50)
     maxRadius = int(ANALYSIS_SIZE)
-    cv.imshow("windowName", analyzerImageGray)
 
     # find the circles
     before = cv.getTickCount()
@@ -117,6 +118,81 @@ if __name__ == '__main__':
     if key == KEY_ESC:
         cv.destroyAllWindows()
         sys.exit(0)
+
+    lineAnalyzerImg = cv.cvtColor(imgShow, cv.COLOR_BGR2GRAY)
+    lineAnalyzerImg = cv.GaussianBlur(lineAnalyzerImg, (7, 7), 0)
+
+    # using this Stack Overflow answer as a reference:
+    #  https://stackoverflow.com/a/45560545/475067
+    lowThreshold = 50
+    highThreshold = 150
+    edges = cv.Canny(lineAnalyzerImg, lowThreshold, highThreshold)
+    edgeDisplay = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
+    rho = 1
+    theta = np.pi / 180
+    threshold = 15
+    minLineLength = 50
+    maxLineGap = 20
+
+    lines = cv.HoughLinesP(edges, rho, theta, threshold, np.array([]), minLineLength=minLineLength, maxLineGap=maxLineGap)
+    print(len(lines))
+    lineList = {}
+    for line in lines[:20]:
+        for (x1, y1, x2, y2) in line:
+            dx = x2 - x1
+            dy = y2 - y1
+            theta = round(np.arctan(dy/dx) * 180/np.pi)
+            if theta not in lineList:
+                lineList[theta] = { 'lines': set(), 'total': 0.0 }
+            lineList[theta]['lines'].add((x1, y1, x2, y2))
+            lineList[theta]['total'] += np.sqrt(np.square(dx) + np.square(dy))
+    
+    lineListByLength = {}
+    for angle in lineList.keys():
+        lineItem = lineList[angle]
+        lineListByLength[lineItem['total']] = { 'lines': lineItem['lines'], 'angle': angle }
+    
+    lengths = lineListByLength.keys()
+    lengths = sorted(lengths)
+    lengths.reverse()
+    print(len(lengths), lengths)
+
+    # input loop
+    index = 0
+    displayLines = False
+    while True:
+        if displayLines:
+            imgDisplay = edgeDisplay.copy()
+        else:
+            imgDisplay = imgShow.copy()
+        for line in lineListByLength[lengths[index]]['lines']:
+            (x1, y1, x2, y2) = line
+            cv.line(imgDisplay, (x1, y1), (x2, y2), (0, 0, 255), 5)
+        (rows, cols, _) = imgDisplay.shape
+        M = cv.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),lineListByLength[lengths[index]]['angle'],1)
+        imgDisplay = cv.warpAffine(imgDisplay, M, (cols, rows))
+        cv.imshow(windowName, imgDisplay)
+
+        key = cv.waitKeyEx(0)
+        if 32 < key < 128:
+            key = chr(key).upper()
+        
+        if key == KEY_ESC:
+            break
+        elif key == KEY_ENTER:
+            break
+        elif key == KEY_SPACE:
+            print("HEY")
+            displayLines = not displayLines
+
+        elif key == 'W':
+            # select previous line
+            if index > 0:
+                index -= 1
+        elif key == 'S':
+            # select next line
+            if index < len(lengths)-1:
+                index += 1
 
     # clean up
     cv.destroyAllWindows()
