8868518e69f441a8866674899148e88c25e6b8b5
Mike Melanson <mike@multimedia.cx>
2021-05-06 21:42:44 -0700

    Simplify logic

--- workflow.py
+++ workflow.py
@@ -11,23 +11,13 @@ KEY_UP_ARROW = 0x260000
 KEY_RIGHT_ARROW = 0x270000
 KEY_DOWN_ARROW = 0x280000
 
-windowName = 'Process Image'
+windowName = 'MobyFlow'
 
 # would be nice to detect these parameters somehow
 maxWidth = 3700
 maxHeight = 2000
 monitorScaleFactor = 1.75
 ANALYSIS_SIZE = 400
-
-
-def plotCircle(img, circle, xRatio, yRatio, windowName):
-    (centerX, centerY, radius) = circle
-    centerX = int(centerX * xRatio)
-    centerY = int(centerY * yRatio)
-    radius = int(radius * min(xRatio, yRatio))
-    imgDisplay = img.copy()
-    cv.circle(imgDisplay, (centerX, centerY), int(radius), (0, 0, 255), 2)
-    cv.imshow(windowName, imgDisplay)
 
 
 if __name__ == '__main__':
@@ -66,10 +56,17 @@ if __name__ == '__main__':
     yRatio = imgShow.shape[1] / analyzerImage.shape[1]
 
     index = 0
-    plotCircle(imgShow, circles[0][index], xRatio, yRatio, windowName)
 
     # input loop
     while True:
+        (centerX, centerY, radius) = circles[0][index]
+        centerX = int(centerX * xRatio)
+        centerY = int(centerY * yRatio)
+        radius = int(radius * min(xRatio, yRatio))
+        imgDisplay = imgShow.copy()
+        cv.circle(imgDisplay, (centerX, centerY), int(radius), (0, 0, 255), 2)
+        cv.imshow(windowName, imgDisplay)
+
         key = cv.waitKeyEx(0)
         if 32 <= key < 128:
             key = chr(key).upper()
@@ -109,11 +106,6 @@ if __name__ == '__main__':
         elif key == KEY_UP_ARROW:
             # move centerpoint up
             circle[1] -= 1
-        
-        circle = circles[0][index]
-        if len(circle) > 0:
-            print("circle %d @ (%d, %d), radius %d" % (index, circle[0], circle[1], circle[2]))
-            plotCircle(imgShow, circle, xRatio, yRatio, windowName)
 
     if key == KEY_ESC:
         cv.destroyAllWindows()
